fa407e7158a0536e686dcf98049daf9a24b10ce5
william.cuesta <william.cuesta@rappi.com>
2023-07-17 01:17:43 -0500

    bugfix/solution_endpoint_dont_response_ok

--- weatherapi/infrastructure/adapters/http_adapter.py
+++ weatherapi/infrastructure/adapters/http_adapter.py
@@ -5,16 +5,9 @@ import logging
 async def request_http_get(url, headers):
     async with aiohttp.ClientSession() as session:
         async with session.get(url, headers=headers) as response:
-            logging.info("Status:", response.status)
-            logging.info("Content-type:", response.headers["content-type"])
-
-            info = await response.json()
-            logging.info("Body:", info)
+            try:
+                info = await response.json()
+                logging.info("Body:", info)
+            except Exception as e:
+                logging.info(e)
             return info
-
-
-async def request_http_post(url, data, headers):
-    async with aiohttp.ClientSession() as session:
-        async with session.post(url, json=data, headers=headers) as response:
-            logging.info(f"Status: {response.status}")
-            logging.info(f"Content-type: {response.headers['content-type']}")
